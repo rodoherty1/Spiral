faa3fdfe75b72149947fe9a6406bddf16acb2c7d
Rob O'Doherty <rodoherty1@gmail.com>
2014-07-04 21:13:31 +0100

    Final tidy up

--- src/main/scala/spiral/Spiral.scala
+++ src/main/scala/spiral/Spiral.scala
@@ -1,14 +1,7 @@
 package spiral
 
-object Spiral {
-    val x = 'x'
-    val o = '.'
-}
-
 case class Spiral(size: Int, points: List[Point]) {
 
-    import spiral.Spiral._
-    
     def addToSpiral(p: Point): Spiral = {
         Spiral(size, p :: points)
     }
@@ -34,24 +27,4 @@ case class Spiral(size: Int, points: List[Point]) {
 
     	List.tabulate(size * size)(n => getCh(n)) mkString ("")
     }
-
-//    override def toString(): String = {
-//        def appendRow (l: List[Char]): String = {
-//            l match {
-//                case (Nil) => "\n"
-//                case (x :: xs) =>
-//                    x + appendRow(xs)
-//            }
-//        }
-//        
-//        def appendRows(rows: List[List[Char]]): String = {
-//	        rows match {
-//	            case (Nil) => ""
-//		        case (xs1 :: xs2) =>
-//		                appendRow(xs1) + appendRows(xs2)
-//		        }
-//	        }
-//
-//        appendRows(s)
-//    }
 }
